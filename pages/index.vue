d2d717b2ee5910023c0dcba5185bbf270e62791c
younoch <younoch295@outlook.com>
2021-12-18 17:34:44 +0600

    page-1

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full main-container">
-    <section class="relative flex justify-center w-full py-10 align-middle">
-      <div class="absolute top-0 left-0 h-full background-1"></div>
+    <!-- Section 1 -->
+    <section
+      class="relative flex justify-center w-full py-10 align-middle part-1"
+    >
       <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
         <aside class="col-span-6 font-medium text-white">
           <h1 class="text-5xl">Atum <span class="underline">Token</span></h1>
@@ -22,26 +24,19 @@
           </div>
         </aside>
         <figure class="col-span-6">
-          <img class="img-para" src="@/assets/images/page1-1.png" alt="" />
+          <img class="img-para" src="@/assets/images/page1-1.png" />
         </figure>
       </main>
     </section>
-
-    <section
-      class="relative flex justify-center w-full py-10 align-middle part-2"
-    >
-      <div class="absolute top-0 left-0 background-2"></div>
+    <!-- Section 2 -->
+    <section class="relative flex justify-center w-full align-middle part-2">
       <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
         <figure class="col-span-6">
-          <img
-            class="img-para"
-            src="@/assets/images/atum_overview.svg"
-            alt=""
-          />
+          <img class="img-para" src="@/assets/images/atum_overview.svg" />
         </figure>
         <aside class="col-span-6 font-medium">
           <h1 class="text-3xl font-semibold">
-            Atum <span class="underline text-grad"> Overview</span>
+            Atum <span class="text-grad"> Overview</span>
           </h1>
 
           <p class="mt-5 text-sm font-normal">
@@ -70,6 +65,291 @@
         </aside>
       </main>
     </section>
+
+    <!-- Section 3 -->
+    <section class="relative flex justify-center w-full align-middle part-3">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <aside class="col-span-6 font-medium">
+          <h1 class="text-3xl font-semibold">
+            The <span class="text-grad"> Metcalfe Effect </span>
+          </h1>
+
+          <p class="mt-5 text-sm font-normal">
+            As ATUM Asset is a platform targeting the mass market, Metcalfe's
+            law (which states the value of a network is proportional to the
+            square of the number of connected users of the system) will play a
+            role in the growth of the user base. <br />
+            ^ Metcalfe’s law in action - Two telephones can make only one
+            connection, ﬁve can make 10 connections, and 12 can make 66
+            connections and so on. There are numerous positives associated with
+            a growth of the ATUM user base, such as:
+          </p>
+          <ul class="mt-2 text-sm font-normal">
+            <li>
+              ● Increase in the scarcity of ATUM digital assets (driving value)
+            </li>
+            <li>● Increased liquidity of the ATUM token</li>
+            <li>● Increase in the number of staked ATUM tokens</li>
+            <li>● Increased activity in the secondary market</li>
+            <li>● Increased social activity across the ATUM Asset app</li>
+            <li>● Increased uptake of new (digital Assetible) releases.</li>
+          </ul>
+        </aside>
+        <figure class="col-span-6">
+          <img class="img-para" src="@/assets/images/the_metcalfe_effect.svg" />
+        </figure>
+      </main>
+    </section>
+    <!-- Section 4 -->
+    <section class="relative flex justify-center w-full align-middle part-4">
+      <img
+        src="@/assets/images/blockchain_nft_tokens_bg1.svg"
+        class="absolute top-0 left-0 sec4-bg"
+      />
+      <img
+        src="@/assets/images/blockchain_nft_tokens_bg2.svg"
+        class="absolute bottom-0 right-0 sec4-bg"
+      />
+
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <figure class="col-span-6">
+          <img
+            class="img-para"
+            src="@/assets/images/blockchain_nft_tokens.svg"
+          />
+        </figure>
+        <aside class="col-span-6 font-medium">
+          <div class="pb-6 my-6 sec4-article">
+            <div class="flex items-center">
+              <figure class="icon-grad">
+                <img src="@/assets/icons/blockchain.png" />
+              </figure>
+              <p class="ml-3 text-xl font-semibold">
+                Why <span class="text-grad"> Blockchain ? </span>
+              </p>
+            </div>
+            <p class="mx-6 mt-6 text-sm font-normal">
+              ATUM employs Blockchain / Distributed Ledger Technology in a
+              number of ways to make the ATUM experience possible.
+            </p>
+          </div>
+          <div class="pb-6 my-6 sec4-article">
+            <div class="flex items-center">
+              <figure class="icon-grad">
+                <img src="@/assets/icons/nft.png" />
+              </figure>
+              <p class="ml-3 text-xl font-semibold">
+                Non Fungible Token <span class="text-grad"> (NFT) </span>
+              </p>
+            </div>
+            <p class="mx-6 mt-6 text-sm font-normal">
+              The digital assets oﬀered through ATUM Asset are NFTs. The NFT
+              standard allows for veriﬁable digital scarcity with a proven
+              authenticity and an immutable proof of ownership. NFT’s allow for
+              more detailed attributes to be included, such as rich metadata
+              about the asset and these authenticated details allow owners to be
+              conﬁdent about the NFT’s authenticity and provenance.
+            </p>
+          </div>
+        </aside>
+      </main>
+    </section>
+    <!-- Section 5 -->
+    <section class="relative flex justify-center w-full align-middle part-5">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <div class="col-span-6 pb-6 my-6 sec5-article">
+          <div class="flex items-center">
+            <figure class="icon-grad">
+              <img src="@/assets/icons/scarcity.png" />
+            </figure>
+            <p class="mx-3 text-xl font-semibold">
+              <span class="text-grad"> Tokens </span>
+            </p>
+          </div>
+          <p class="mx-6 mt-6 text-sm font-normal">
+            ATUM digital assets are released in a limited number of batches,
+            called ‘Series’. A Series may, for example, have ten thousand
+            digital assets and within this Series the digital assets are divided
+            across ﬁve rarity categories, ranging from ‘Common’, (representing
+            62% of the total) through to ‘A-Rare’ (representing 1%). The
+            Blockchain and NFT standard is used to generate these limited number
+            of digital assets.
+          </p>
+        </div>
+        <div class="col-span-6 pb-6 my-6 sec5-article">
+          <div class="flex items-center">
+            <figure class="icon-grad">
+              <img src="@/assets/icons/transfer.png" />
+            </figure>
+            <p class="mx-3 text-xl font-semibold">
+              Peer-to-Peer
+              <span class="text-grad"> Transfer </span>
+            </p>
+          </div>
+          <p class="mx-6 mt-6 text-sm font-normal">
+            To facilitate eﬀortless buying, selling and trading of digital
+            assets amongst users within the app, ATUM Asset leverages blockchain
+            for the trustless transfer of digital assets between users. When a
+            transfer happens, ownership is immediately transferred to the new
+            owner and the previous owner will no longer have access to the
+            digital Asset.
+          </p>
+        </div>
+        <div class="col-span-6 pb-6 my-6 sec5-article">
+          <div class="flex items-center">
+            <figure class="icon-grad">
+              <img src="@/assets/icons/transfer.png" />
+            </figure>
+            <p class="mx-3 text-xl font-semibold">
+              ATUM
+              <span class="text-grad"> Asset </span>
+            </p>
+          </div>
+          <p class="mx-6 mt-6 text-sm font-normal">
+            Assets have existed throughout human history and today they come in
+            many forms. Currently the virtual goods market is valued at
+            $52Billion annually and growing rapidly.
+          </p>
+        </div>
+        <div class="col-span-6 pb-6 my-6 sec5-article">
+          <div class="flex items-center">
+            <figure class="icon-grad">
+              <img src="@/assets/icons/transfer.png" />
+            </figure>
+            <p class="mx-3 text-xl font-semibold">
+              Counterfeit
+              <span class="text-grad"> Protection </span>
+            </p>
+          </div>
+          <p class="mx-6 mt-6 text-sm font-normal">
+            A key advantage of using the Blockchain for digital assets is that
+            each Asset is a uniquely generated asset that cannot be duplicated.
+            Each digital Asset is recorded in the Blockchain and ties ownership
+            directly to the veriﬁed owner.
+          </p>
+        </div>
+      </main>
+    </section>
+    <!-- Section 6 -->
+    <section class="relative flex justify-center w-full align-middle part-6">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <aside class="col-span-6 font-medium">
+          <h1 class="text-3xl font-semibold">
+            <span class="text-grad"> Overview </span>
+          </h1>
+
+          <p class="mt-5 text-sm font-normal">
+            ATUM is a cross-platform (mobile, desktop, web) application used for
+            the delivery and storage of digital Asset content. It is a
+            marketplace where users can buy, swap, sell and store digital assets
+            in the form of digital Asset NFTs. The authenticity, scarcity and
+            ownership of digital assets are managed using Distributed Ledger
+            Technology.
+          </p>
+        </aside>
+        <figure class="col-span-6">
+          <img class="img-para" src="@/assets/images/overview.svg" />
+        </figure>
+      </main>
+    </section>
+    <!-- Section 7 -->
+    <section class="relative flex justify-center w-full align-middle part-7">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <figure class="col-span-6">
+          <img class="img-para" src="@/assets/images/technology.png" />
+        </figure>
+        <aside class="col-span-6 font-medium">
+          <h1 class="text-3xl font-semibold">
+            <span class="text-grad"> Technology </span>
+          </h1>
+
+          <p class="mt-5 text-sm font-normal">
+            <span class="font-semibold"
+              >Blockchain Protocol and Contract Standards</span
+            >
+            <br />
+            Due to the throughput and congestion issues that currently plague
+            the Ethereum network, ATUM Asset will be deploying the ATUM token
+            and the digital Asset NFT contracts on the network. The ATUM NFT
+            smart contract will be compatible with the ERC20 token standard.
+            <br />
+            <br />
+            <span class="font-semibold">Minting and Generation</span> <br />
+            When new digital assets are created they are done so by minting a
+            new NFT in our blockchain contract. Each NFT is provided with an
+            associated URI which links that NFT to a set of metadata which
+            includes the Asset name, licensor, brand, series and links to the
+            NFT’s various digital assets (such as clothing, props, etc). ATUM
+            will host the publicly accessible JSON API containing the metadata
+            for each NFT’s URI. Digital Exchange Platform and NFT Ownership
+            After the minting of each NFT, the ownership is sent to an address
+            assigned to ATUM for security and management on behalf of the owner.
+            The ATUM Asset platform operates as a hybrid model whereby ATUM
+            maintains ownership of all assets within the system, as is a common
+            practice among digital exchange platforms. The ATUM Asset platform
+            will maintain centralized ownership records as users buy, sell and
+            trade NFTs. Additionally, all transactions in our system require
+            signing authorization from either the user’s app or their Asset
+            Digital Hardware Wallet. This hybrid approach allows ATUM to provide
+            users with a streamlined experience within the ATUM Asset app, while
+            still leveraging the power of Distributed Ledger Technology for ATUM
+            digital assets.
+          </p>
+        </aside>
+      </main>
+    </section>
+    <!-- Section 8 -->
+    <section class="relative flex justify-center w-full align-middle part-8">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <aside class="col-span-6 font-medium">
+          <h1 class="text-3xl font-semibold">
+            <span class="text-grad"> Onboarding </span>
+          </h1>
+
+          <p class="mt-5 text-sm font-normal">
+            Registration is required to use and access the ATUM Asset app. /site
+            To facilitate simple onboarding, ATUM Asset will oﬀer Single Sign On
+            Services with an option to sign up manually via email. During
+            onboarding, the user will be prompted to indicate their preferences
+            towards speciﬁc brands and interests, which is used as a basis for
+            personalized content.
+          </p>
+        </aside>
+        <figure class="col-span-6">
+          <img class="img-para" src="@/assets/images/onboarding.png" />
+        </figure>
+      </main>
+    </section>
+    <!-- Section 9 -->
+    <section class="relative flex justify-center w-full align-middle part-8">
+      <main class="container grid items-center grid-cols-12 gap-5 py-6 mx-40">
+        <figure class="col-span-6">
+          <img class="img-para" src="@/assets/images/token.png" />
+        </figure>
+        <aside class="col-span-6 font-medium">
+          <h1 class="text-3xl font-semibold">
+            <span class="text-grad"> Token </span>
+          </h1>
+
+          <p class="mt-5 text-sm font-normal">
+            The underlying economy of ATUM Asset is the ATUM Token. The token is
+            needed for two reasons. Firstly, the sale and purchase of digital
+            assets and secondly, access to extra features and beneﬁts within the
+            app, such as the ’Master Asset Program’. When a purchase is made
+            using ATUM tokens, the tokens involved in the transaction are
+            exchanged for the digital Asset and then expire. After a successful
+            purchase the invalid ATUM tokens are sent to an inaccessible smart
+            contract where they reside permanently. ATUM Asset users can acquire
+            ATUM Tokens using a credit or debit card, or where applicable, via
+            an in-app purchase. When via in-app purchases, the payment will
+            seamlessly purchase an amount of ATUM tokens (determined by the
+            user) and once the ATUMs are acquired they are used to purchase the
+            digital Asset. This process happens entirely in the background.
+          </p>
+        </aside>
+      </main>
+    </section>
+    <Footer />
   </div>
 </template>
 
@@ -96,11 +376,22 @@ export default {
   height: 576px;
 }
 .text-grad {
-  background: -webkit-linear-gradient(#2842ef 0%, #a25ffd 100%);
-  -webkit-background-clip: text;
+  background-image: -webkit-linear-gradient(#2842ef 0%, #a25ffd 100%);
+  border-bottom: 2.5px solid #2842ef;
+  -webkit-background-clip: text, border;
   -webkit-text-fill-color: transparent;
 }
-.background-1 {
+.icon-grad {
+  @apply flex align-middle justify-center px-1 py-2;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    linear-gradient(133.33deg, #2842ef 0%, #a25ffd 100%);
+  width: fit-content;
+}
+.icon-grad img {
+  width: 80%;
+}
+/* Section 1  */
+.part-1 {
   @apply w-full;
 
   z-index: -1;
@@ -112,11 +403,26 @@ export default {
     #2842ef 370.72deg
   );
 }
-.background-2 {
-  @apply w-full h-full;
+/* section 2 */
+.part-2 {
   background-image: url(assets\images\atum_overview_bg.svg);
   background-repeat: no-repeat;
   background-size: 100% 100%;
+}
+/* section 3 */
+.part-3 {
+  background: #fcfcfc;
+}
+.part-2 p,
+.part-3 p,
+.part-4 p,
+.part-5 p,
+.part-6 p,
+.part-7 p,
+.part-8 p,
+.part-9 p {
+  line-height: 24px;
+  color: #121212;
 }
 button {
   @apply w-28 py-3 px-4 mr-3;
@@ -131,6 +437,38 @@ button {
 }
 .img-para {
   height: 480px;
+}
+/* section 3 */
+.part-3 {
+  background-image: url(assets\images\the_metcalfe_effect_bg.svg);
+}
+/* section 4 */
+.sec4-bg {
+  height: 80%;
+  z-index: -1;
+}
+.sec4-article {
+  background: #fbf9fe;
+}
+/* section 5 */
+.part-5 {
+  background: #e5e5e5;
+}
+.sec4-article {
+  background: white;
+}
+
+/* section 6 */
+.part-6 {
+  background: #e5e5e5;
+}
+/* section 7 */
+.part-7 {
+  background: #e5e5e5;
+}
+/* section 8 */
+.part-8 {
+  background: #e5e5e5;
 }
 @media (min-width: 768px) {
   .container {
